d1f8a35acbdd349d33f8b98befbcebcb74d9c115
jim.an <jianyu234@gmail.com>
2025-01-31 17:58:08 -0500

    updated traversability_script.py: mapping depth and traversability back to original lidar points

--- traversability_script.py
+++ traversability_script.py
@@ -10,15 +10,13 @@ from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
 from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation
 """
 
-traverse dict for "facebook/mask2former-swin-small-cityscapes-semantic"
-{0: 'road', 1: 'sidewalk', 2: 'building', 3: 'wall', 4: 'fence', 5: 'pole', 6: 'traffic light', 7: 'traffic sign', 8: 'vegetation', 9: 'terrain', 10: 'sky', 11: 'person', 12: 'rider', 13: 'car', 14: 'truck', 15: 'bus', 16: 'train', 17: 'motorcycle', 18: 'bicycle'}
-note "facebook/mask2former-swin-large-cityscapes-instance" does worse?
+
 
 """
 img_pkl_path = f"/home/jim/Documents/Projects/GND_alignment/new_AU/AU/AU"
 point_cloud_path = f"/home/jim/Documents/Projects/GND_alignment/AU/Scans"
 times_txt_path = f"/home/jim/Documents/Projects/GND_alignment/AU/times.txt"
-pretrain_model = "segformer" # select mask2former or segformer
+pretrain_model = "mask2former" # select mask2former or segformer
 
 # GMU intrinstic
 intrinsic = [[248.621049856283, 0, 333.674701475662],
@@ -64,21 +62,38 @@ traversability_dict = {  # applies for both mask2former and segformer
 
 
 def lidar_to_egoview(image, pcd_points, intrinsic, extrinsic):
+    """
+
+    :param image: (h, w, 3), typically (360 x 640)
+    :param pcd_points: (n_points, 3)
+    :param intrinsic: list of lists, (3, 3)
+    :param extrinsic: list of lists, (4, 4)
+    :return:
+        depth_image:
+        refined_pcd:
+        refined_color:
+    """
     image = np.array(image)
     image_size = image.shape
     H, W, D = image_size
     depth_image = np.full((H, W), np.inf)
+    image_map = np.full((H, W), -1)
     # Convert point cloud to homogeneous coordinates
+    # (n_points, 4)
     points_hom = np.hstack((pcd_points, np.ones((pcd_points.shape[0], 1))))
     # Transform to camera coordinates
-    points_cam = (extrinsic @ points_hom.T).T
-    points_cam = points_cam[points_cam[:, 2] > 0]  # Filter points with z > 0
+    points_cam = (extrinsic @ points_hom.T).T # (n_points, 4)
+    subset_mapping = points_cam[:, 2] > 0 # Filter points with z > 0
+    # -> indicies of each point as if they are in the original pcd_points data!
+    match_points_to_subset = np.arange(0, points_cam.shape[0])[subset_mapping] # (n_subset, 4)
+    points_cam = points_cam[subset_mapping] # (n_subset, 4)
     # print(points_hom)
-    raw_pcd = (np.linalg.inv(extrinsic) @ points_cam.T).T
+    raw_pcd = (np.linalg.inv(extrinsic) @ points_cam.T).T  # (n_subset, 4)
     # Project to image plane
-    pixels_hom = (intrinsic @ points_cam[:, :3].T).T
-    pixels = pixels_hom[:, :2] / pixels_hom[:, 2:]
+    pixels_hom = (intrinsic @ points_cam[:, :3].T).T  # (n_subset, 3)
+    pixels = pixels_hom[:, :2] / pixels_hom[:, 2:] # (n_subset, 2)
     refined_pcd = []
+    refined_pcd_mapping = []
     refined_color = []
     # Rasterize depth image
     for i, (u, v) in enumerate(pixels):
@@ -86,14 +101,15 @@ def lidar_to_egoview(image, pcd_points, intrinsic, extrinsic):
         if 0 <= u < W and 0 <= v < H:
             z = points_cam[i, 2]
             depth_image[v, u] = min(depth_image[v, u], z)
-
+            image_map[v, u] = match_points_to_subset[i]
             refined_pcd.append(raw_pcd[i, :])
+            refined_pcd_mapping.append(match_points_to_subset[i])
             refined_color.append([u, v])
     # Replace inf with 0 (or a desired background value)
     depth_image[depth_image == np.inf] = 0
     refined_pcd = np.array(refined_pcd)[:, :3]
     refined_color = np.array(refined_color)
-    return depth_image, refined_pcd, refined_color
+    return depth_image, image_map, refined_pcd, np.array(refined_pcd_mapping), refined_color
 
 # Semantic segmentation from the same dataset:
 print(f"using {pretrain_model} model")
@@ -104,7 +120,14 @@ elif pretrain_model == "mask2former":
     processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-large-cityscapes-panoptic")
     model = Mask2FormerForUniversalSegmentation.from_pretrained("facebook/mask2former-swin-large-cityscapes-panoptic")
 
-color_palette = [list(np.random.choice(range(256), size=3)) for _ in range(len(traverse_def_dict))]
+color_palette = [
+    [47, 171, 97],          # 1 green?   side walk or other traversable areas for pedestrians
+    [200,  0,  0],          # 2 blue?    the parking lot, drive way for vehicles
+    [126, 49, 141],         # 3 purple?  off-road including vegetation, grass, mud etc
+    [55, 128, 235],         # 4          stairs, curbs etc for legged robot only
+    [8, 149, 174],          # 5 yellow?  obstacles or buildings
+    [0, 0, 0],              # 6 nocolor. do not segment, discard
+]
 color_palette = np.array(color_palette)
 
 for i, (img_dir, lidar_dir) in enumerate(zip(os.listdir(img_pkl_path), os.listdir(point_cloud_path))):
@@ -128,7 +151,8 @@ for i, (img_dir, lidar_dir) in enumerate(zip(os.listdir(img_pkl_path), os.listdi
         continue
     image = Image.fromarray(img[0])
     # get depth image from lidar transformation
-    depth_image, refined_pcd, refined_color = lidar_to_egoview(image, pcd_points, intrinsic, extrinsic)
+    depth_image, image_lidar_map, refined_pcd, pcd_lidar_map, refined_color = lidar_to_egoview(image, pcd_points, intrinsic, extrinsic)
+    # depth_image, refined_pcd, refined_color = lidar_to_egoview(image, pcd_points, intrinsic, extrinsic)
 
 
     # semantic segmentation
@@ -143,44 +167,55 @@ for i, (img_dir, lidar_dir) in enumerate(zip(os.listdir(img_pkl_path), os.listdi
     assert len(traversability_dict) == len(model.config.id2label)
     # convert segmentation results according to traversability_dict
     segmentation = results.detach().cpu().numpy()
+    segmentation_copy = segmentation.copy()
 
     color_seg = np.zeros((segmentation.shape[0], segmentation.shape[1], 3), dtype=np.uint8)
-    for key, value in traversability_dict.items():
-        segmentation[segmentation == key] = value
+    view_mode = "traversability" # "segmentation" or "traversability"
+    if view_mode == "traversability":
+        for key, value in traversability_dict.items():
+            segmentation[segmentation_copy == key] = value
+        lookup_dict = traverse_def_dict
+    elif view_mode == "segmentation":
+        lookup_dict = model.config.id2label
 
-    print(segmentation.shape)
     i = 0
-    for label, definition in traverse_def_dict.items():
-        # text_label = model.config.id2label[label]
+    for label, definition in lookup_dict.items():
         text_label = definition
         # overlay
-        color_seg[segmentation == label, :] = color_palette[i]
+        color_seg[segmentation == label, :] = color_palette[i % len(color_palette)]
 
-        # Show image + mask
-        mask = (segmentation == label)
-        visual_mask = (mask * 255).astype(np.uint8)
-        visual_mask = Image.fromarray(visual_mask)
+        # !! uncomment below lines to show image + mask
+        # mask = (segmentation == label)
+        # visual_mask = (mask * 255).astype(np.uint8)
+        # visual_mask = Image.fromarray(visual_mask)
         # cv2.imshow(f"{text_label} mask", np.array(visual_mask))
         i+=1
     seg_overlay = np.array(image) * 0.5 + color_seg * 0.5
     seg_overlay = seg_overlay.astype(np.uint8)
     cv2.imshow(f"overlay segmentation", seg_overlay)
-    cv2.imshow("input image", np.array(image))
-    cv2.imshow("depth image", depth_image)
+    # cv2.imshow("input image", np.array(image))
+    # cv2.imshow("depth image", depth_image)
+    depth_mask = depth_image > 0
+    depth_overlay = [depth_mask * 255 for i in range(3)]
+    depth_highlight_img = np.stack(depth_overlay, axis=2) * 0.5 + np.array(image) * 0.5
+    cv2.imshow("depth overlay", depth_highlight_img.astype(np.uint8))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    # now that we have the depth, I match the depth information back to the lidar as an extra dimension.
+    combined_list = []
+    for i in range(segmentation.shape[0]):
+        for j in range(segmentation.shape[1]):
+            if bool(depth_mask[i][j]):
+                lidar_entry = image_lidar_map[i][j]
+                fused_data = pcd_points[lidar_entry]
+                fused_data = np.append(fused_data, np.array([segmentation[i][j], depth_image[i][j]]))
+                combined_list.append(fused_data)
+    print("lidar_x; lidar_y; lidar_z; traversability_category; depth_value")
+    print(combined_list[0])
     break
 
 
 
 
-
-
-
-
-
-
-# image shape: (H, W, C) where H=480, W=640
-# segmentation shape: (H, W), encoded 1 through 6
-# depth_matrix shape? (H, W, D)? where D could range from 0 to 10
 
